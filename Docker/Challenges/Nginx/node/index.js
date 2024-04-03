4882667747b247773c96231b6e186a4086cef37f
walopes <walopes23@gmail.com>
2024-04-03 12:55:21 -0300

    Added node files for Nginx challenge

--- Docker/Challenges/Nginx/node/index.js
+++ Docker/Challenges/Nginx/node/index.js
@@ -7,24 +7,44 @@ const config = {
     password: 'root',
     database: 'nodedb'
 };
+
+const myName = 'Willian';
+
 const mysql = require('mysql')
 const connection = mysql.createConnection(config);
-// connection.beginTransaction()
-// const createDb = `CREATE TABLE People(Id int autoincrement, )`;
-// connection.query(createDb);
+var sql = `CREATE TABLE IF NOT EXISTS People(
+    Id int auto_increment,
+    Name varchar(30),
+    primary key (Id)
+)`;
+connection.query(sql);
 
-const sql = `SELECT Name FROM People limit 1`;
-var name = '';
-connection.query(sql, function(err, result, fields){
-    if(err) throw err;
-    name = result[0].Name;
-});
+sql = `INSERT INTO People(Name) Values('${myName+(new Date().getMilliseconds())}');`;
+connection.query(sql);
 connection.end();
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Full Cycle: ${name}</h1>`)
+
+    const mysql = require('mysql')
+    const connection = mysql.createConnection(config);
+    const sql = `SELECT Name FROM People`;
+    var names = []
+    connection.query(sql, function(err, result, fields){
+        if(err) throw err;
+        result.forEach(e => names.push(e.Name));
+        var storedNames = [];
+        for(var name of names)
+            storedNames.push(`<h4>${name}</h4>`);
+        res.send(`
+            <h1>Full Cycle Rocks!</h1>
+            <br/>
+            <h2>Now, the names inserted...</h2>
+            ${storedNames.join('<br/>')}`
+            )
+        });
+    connection.end();
 })
 
 app.listen(port, () => {
-    console.log('Rodando na porta ' + port)
+    console.log('Running at port ' + port)
 });
